6cbfc2349fa34f0d3f20bbd0c1b26a9940a730d0
chiemerieani257-create <chiemerieani257@gmail.com>
2025-12-21 13:48:21 +0100

    Add approval function for payment processing

---
+++ netlify/functions/approve.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event) => {
+  const { paymentId } = JSON.parse(event.body);
+
+  await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
+    method: "POST",
+    headers: {
+      Authorization: `Key ${process.env.PI_API_KEY}`
+    }
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ status: "approved" })
+  };
+};
